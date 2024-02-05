9555437e0ca90c6247745a27e63a3aa8eac791fc
Luis Mendes <machadoluismendes@gmail.com>
2024-02-05 11:38:20 -0300

    Increment
    mongoose connect exception handling

--- server.tsx
+++ server.tsx
@@ -2,51 +2,53 @@
 
 import mongoose from "mongoose";
 
-  const express = require('express');
-  const app = express();
-  // const mongoose:any = require('mongoose');
-  const bodyParser = require('body-parser');
-  const cors = require('cors');
-  require('dotenv/config');
-  
-  const fs = require('fs');
-  const csv = require('csv-parser');
-  
-  // Define your City model as usual
-  const City = mongoose.model('City', require('./app/models/City.tsx'));
-  
-  fs.createReadStream('uscities-data.csv')
-    .pipe(csv())
-    .on('data', (row: any) => {
-      // Create a new city instance for each row in the CSV
-      const city = new City(row);
-      try{
+const express = require("express");
+const app = express();
+// const mongoose:any = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+
+const fs = require("fs");
+const csv = require("csv-parser");
+
+// Define your City model as usual
+const City = mongoose.model("City", require("./app/models/City.tsx"));
+
+fs.createReadStream("uscities-data.csv")
+  .pipe(csv())
+  .on("data", (row: any) => {
+    // Create a new city instance for each row in the CSV
+    const city = new City(row);
+    try {
       // Save it to the database
       city.save();
-    }catch(error:any){
+    } catch (error: any) {
       console.error("outer", error.message);
-    }finally{
-    
+    } finally {
     }
-    })
-    .on('end', () => {
-      console.log('CSV file successfully processed');
-    });
-  
-  
-  app.use(cors());
-  app.use(bodyParser.json());
-  
-  // Import Routes
-  const cityRoute = require('./routes/cities');
-  app.use('/state/city', cityRoute);
-  
+  })
+  .on("end", () => {
+    console.log("CSV file successfully processed");
+  });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Import Routes
+const cityRoute = require("./routes/cities");
+app.use("/state/city", cityRoute);
+
+try {
   // Connect to DB
   mongoose.connect(
     process.env.MONGODB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log('connected to DB!')
+    () => console.log("connected to DB!")
   );
-  
-  app.listen(3000);
+} catch (error:any) {
+  console.log(error.message);
+} finally {
+}
 
+app.listen(3000);
